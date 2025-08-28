a407c47b71e6aecde735ef1775a30d3907cd53fa
Dan Russo <rdaniele669@gmail.com>
2025-08-28 15:35:38 +0200

    adds second paragraph of Cras page and some fixes

--- Frontend/src/components/Cras.tsx
+++ Frontend/src/components/Cras.tsx
@@ -1,18 +1,19 @@
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Cras = () => {
   return (
     <>
-      <Container className="d-flex flex-column justify-content-center align-items-start information my-4">
+      <Container className="cras-section d-flex flex-column justify-content-center align-items-start information my-4 ">
         <h2 className="mx-auto mb-2">Centro di Recupero per Animali Selvatici</h2>
-        <p>
+        <p className="ms-sm-3 ms-md-5">
           Quando si parla di <span className="fw-bold">C.R.A.S.</span> ci si riferisce a strutture, solitamente gestite da enti come Regione, Comuni, enti parco
           o associazioni (WWF, ENPA), che si occupano del recupero, della cura e della riabilitazione degli animali selvatici autoctoni trovati in difficoltà.
           <br /> L'obiettivo principale è restituire la libertà agli animali selvatici, dopo averli curati, riabilitati e aver verificato la loro idoneità per
           il rientro in natura.
         </p>
-        <h4 className="mt-4 mb-2">Come operiamo</h4>
-        <p>
+        <h4 className="mt-4 mb-2 ms-sm-3 ms-md-5 fw-semibold">Come operiamo</h4>
+        <p className="ms-sm-3 ms-md-5">
           Il cittadino svolge un ruolo cruciale come primo anello della catena di pronto intervento. Segnalando tempestivamente il ritrovamento di un animale
           ferito o in difficoltà al <span className="fw-bold">numero unico di emergenza 112 o al nostro numero dedicato</span>, consente ai volontari di
           intervenire in modo rapido e preciso. Spesso una chiamata può essere molto incisiva sulla sopravvivenza dell'animale anche a seconda del tempo di
@@ -29,8 +30,28 @@ const Cras = () => {
           piccola taglia, chi ritrova l’animale deve occuparsi di raccoglierlo e portarlo direttamente al rifugio.
           <br />
           <br />
-          Per ogni animale selvatico soccorso viene realizzata una scheda di accettazione, visibile anche dalla persona che ha richiesto l’intervento, che
-          permette non solo una gestione puntuale di ogni singolo paziente, ma anche di avere sempre a disposizione la situazione complessiva.
+          Per ogni animale selvatico soccorso viene realizzata una <span className="fw-bold">scheda di accettazione</span>, visibile anche dalla persona che ha
+          richiesto l’intervento, che permette non solo una gestione puntuale di ogni singolo paziente, ma anche di avere sempre a disposizione la situazione
+          complessiva.
+        </p>
+        <h4 className="mt-4 mb-2 ms-sm-3 ms-md-5 fw-semibold">Cosa fare in caso di ritrovamento di un animale</h4>
+        <p className="ms-sm-3 ms-md-5">
+          Leggi i nostri{" "}
+          <Link className="cras-links" to={"/cosa-fare"}>
+            approfondimenti
+          </Link>{" "}
+          per affrontare una situazione di emergenza nel modo corretto oppure consulta le{" "}
+          <Link className="cras-links" to={"/faq"}>
+            domande frequenti
+          </Link>{" "}
+          che ci vengono poste.
+          <br />
+          <br />
+          <span className="fw-bold">Nota molto importante</span>: controlla{" "}
+          <Link className="cras-links" to={"https://elencocras.it/"}>
+            il C.R.A.S. di competenza
+          </Link>{" "}
+          nella zona dove trovi l'animale.
         </p>
       </Container>
     </>
--- Frontend/src/components/MyNavbar.tsx
+++ Frontend/src/components/MyNavbar.tsx
@@ -23,9 +23,11 @@ const MyNavbar = () => {
   return (
     // TODO - Collegare i link alle rispettive pagine una volta create.
     <>
-      <Navbar expand="lg" className={`my-nav ${isHomepage ? "fixed-top" : "sticky-top"} ${scrolling ? "scrolled-navbar" : ""}`}>
+      <Navbar expand="lg" className={` ${isHomepage ? "fixed-top my-nav" : "sticky-top my-secondary-nav"} ${scrolling ? "scrolled-navbar" : ""}`}>
         <Container>
-          <Image src={logo} className="logo d-lg-none m-auto" />
+          <Link to={"/"} className="m-auto">
+            <Image src={logo} className="logo d-lg-none" />
+          </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
