a9b8d2419c82f02ecf95ba13ae0bbeed8f3e31e7
Dan Russo <rdaniele669@gmail.com>
2025-09-03 16:20:46 +0200

    removing entities

--- Backend/src/main/java/danrusso/capstoneProject/repositories/ProvinceRepository.java
+++
@@ -1,9 +0,0 @@
-package danrusso.capstoneProject.repositories;
-
-import danrusso.capstoneProject.entities.Province;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProvinceRepository extends JpaRepository<Province, Long> {
-}
--- Backend/src/main/java/danrusso/capstoneProject/repositories/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package danrusso.capstoneProject.repositories;
-
-import danrusso.capstoneProject.entities.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
