05f3ec6c5b6e04ed02c1d0b04df93ff5ab5d4153
Dan Russo <rdaniele669@gmail.com>
2025-09-08 14:52:01 +0200

    adds profile links and logout button

--- Frontend/src/components/Navbar/MyNavbar.tsx
+++ Frontend/src/components/Navbar/MyNavbar.tsx
@@ -1,14 +1,22 @@
-import { Button, Col, Image, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Button, Col, Dropdown, Image, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import logo from "../../assets/Logo-Rifugio-Mamo.jpg";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./navbar.css";
+import { ArrowRightShort } from "react-bootstrap-icons";
 
 const MyNavbar = () => {
   const [scrolling, setScrolling] = useState(false);
   const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
   const location = useLocation();
   const isHomepage = location.pathname === "/";
+
+  const handleShowOffcanvas = () => {
+    setShowOffcanvas(!showOffcanvas);
+    if (!showOffcanvas) setShowProfile(false);
+  };
+  const handleShowProfile = () => setShowProfile(!showProfile);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -17,11 +25,12 @@ const MyNavbar = () => {
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 992 && showOffcanvas) {
-        setShowOffcanvas(false);
+        handleShowOffcanvas();
       }
     };
 
     window.addEventListener("resize", handleResize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [showOffcanvas]);
 
   const handleScroll = () => {
@@ -32,8 +41,10 @@ const MyNavbar = () => {
     }
   };
 
-  const handleClose = () => setShowOffcanvas(false);
-  const handleShow = () => setShowOffcanvas(true);
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    handleShowOffcanvas();
+  };
 
   return (
     // TODO - Collegare i link alle rispettive pagine una volta create.
@@ -42,10 +53,10 @@ const MyNavbar = () => {
         expand="lg"
         className={` ${isHomepage ? "fixed-top my-nav" : "sticky-top my-secondary-nav"} ${scrolling ? "scrolled-navbar" : ""} d-flex justify-content-center`}
       >
-        <Link to={"/"} className="">
+        <Link to={"/"}>
           <Image src={logo} className="logo d-lg-none" />
         </Link>
-        <Button variant="outline-dark" onClick={handleShow} className="d-lg-none position-absolute end-0 me-3">
+        <Button variant="outline-dark" onClick={handleShowOffcanvas} className="d-lg-none position-absolute end-0 me-3">
           ☰
         </Button>
         <Nav as={Col} lg={5} className="d-none d-lg-flex justify-content-end">
@@ -80,47 +91,84 @@ const MyNavbar = () => {
           <Link to={"/visite"} className="nav-link">
             Visite
           </Link>
-          <Link to={"/accedi"} className="nav-link" onClick={handleClose}>
-            Accedi
-          </Link>
+          {localStorage.getItem("accessToken") ? (
+            <Dropdown>
+              <Dropdown.Toggle className="nav-link profile" id="dropdown-basic">
+                Profilo
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="/profilo">I tuoi dati</Dropdown.Item>
+                <Dropdown.Item href="/profilo/adozioni">Le tue adozioni</Dropdown.Item>
+                <Dropdown.Item href="/">Logout</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          ) : (
+            <Link to={"/accedi"} className="nav-link" onClick={handleShowOffcanvas}>
+              Accedi
+            </Link>
+          )}
         </Nav>
       </Navbar>
 
-      <Offcanvas show={showOffcanvas} onHide={handleClose} placement="end">
+      <Offcanvas show={showOffcanvas} onHide={handleShowOffcanvas} placement="end">
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>
-            <Image src={logo} alt="logo" className="logo" />
+            <Link to={"/"}>
+              <Image src={logo} alt="logo" className="logo" onClick={handleShowOffcanvas} />
+            </Link>
           </Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <Nav className="flex-column">
-            <Link to={"/il-rifugio"} className="nav-link" onClick={handleClose}>
+            <Link to={"/il-rifugio"} className="nav-link" onClick={handleShowOffcanvas}>
               Il Rifugio
             </Link>
-            <Link to={"/cras"} className="nav-link" onClick={handleClose}>
+            <Link to={"/cras"} className="nav-link" onClick={handleShowOffcanvas}>
               Il C.R.A.S.
             </Link>
-            <Link to={"/news"} className="nav-link" onClick={handleClose}>
+            <Link to={"/news"} className="nav-link" onClick={handleShowOffcanvas}>
               News
             </Link>
-            <Link to={"/blog"} className="nav-link" onClick={handleClose}>
+            <Link to={"/blog"} className="nav-link" onClick={handleShowOffcanvas}>
               Blog
             </Link>
-            <Link to={"/faq"} className="nav-link" onClick={handleClose}>
+            <Link to={"/faq"} className="nav-link" onClick={handleShowOffcanvas}>
               FAQ
             </Link>
-            <Link to={"/donazioni"} className="nav-link" onClick={handleClose}>
+            <Link to={"/donazioni"} className="nav-link" onClick={handleShowOffcanvas}>
               Donazioni
             </Link>
-            <Link to={"/volontariato"} className="nav-link" onClick={handleClose}>
+            <Link to={"/volontariato"} className="nav-link" onClick={handleShowOffcanvas}>
               Volontariato
             </Link>
-            <Link to={"/visite"} className="nav-link" onClick={handleClose}>
+            <Link to={"/visite"} className="nav-link" onClick={handleShowOffcanvas}>
               Visite
             </Link>
-            <Link to={"/accedi"} className="nav-link" onClick={handleClose}>
-              Accedi
-            </Link>
+            {localStorage.getItem("accessToken") ? (
+              <div className="nav-link prova" onClick={handleShowProfile}>
+                Profilo
+                {showProfile && (
+                  <div className="d-flex flex-column mt-2 ps-3 profile-links-wrapper">
+                    <Link to="/profilo" className="nav-link profile-links" onClick={handleShowOffcanvas}>
+                      <ArrowRightShort className="mb-1" />I tuoi dati
+                    </Link>
+                    <Link to="/profilo/adozioni" className="nav-link profile-links" onClick={handleShowOffcanvas}>
+                      <ArrowRightShort className="mb-1" />
+                      Le tue adozioni
+                    </Link>
+                    <Link to="/" className="nav-link profile-links" onClick={handleLogout}>
+                      <ArrowRightShort className="mb-1" />
+                      Logout
+                    </Link>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link to={"/accedi"} className="nav-link" onClick={handleShowOffcanvas}>
+                Accedi
+              </Link>
+            )}
           </Nav>
         </Offcanvas.Body>
       </Offcanvas>
