84474a840828ea3e7d0f238c6ccd4500121e4841
Dan Russo <rdaniele669@gmail.com>
2025-09-19 10:12:35 +0200

    adds style and responsiveness to viewAll table

--- Frontend/src/components/BackOffice/ViewAllPage.tsx
+++ Frontend/src/components/BackOffice/ViewAllPage.tsx
@@ -3,9 +3,10 @@ import { useAppDispatch, useAppSelector } from "../../redux/store";
 import { allAnimalFetch, resetFilters, setPage as setAnimalPage, setSortBy, setSortByDirection } from "../../redux/actions/animalSlice";
 import { allAdoptionFetch, setPage as setAdoptionPage } from "../../redux/actions/adoptionSlice";
 import FilterOffcanvas from "../FilterOffcanvas";
-import { useSearchParams } from "react-router-dom";
-import { ArrowLeftShort, ArrowRightShort, SortAlphaDown, SortAlphaDownAlt, SortNumericDown, SortNumericDownAlt } from "react-bootstrap-icons";
+import { Link, useSearchParams } from "react-router-dom";
+import { ArrowLeftShort, ArrowReturnLeft, ArrowRightShort, SortAlphaDown, SortAlphaDownAlt, SortNumericDown, SortNumericDownAlt } from "react-bootstrap-icons";
 import { useEffect } from "react";
+import "./viewAll.css";
 
 const ViewAllPage = () => {
   const dispatch = useAppDispatch();
@@ -67,6 +68,11 @@ const ViewAllPage = () => {
       <Container fluid id="back-office-viewAll-section" className="navbar-height d-flex flex-column justify-content-start align-items-start information pb-4">
         <h2 className="titles mx-auto mb-2 mt-4">{isAnimalPage ? "Animali" : "Adozioni"}</h2>
         <h4 className="subtitles mx-auto">{isAnimalPage ? "Visualizza tutti gli animali" : "Visualizza tutte le adozioni"}</h4>
+        <Link to={"/back-office"} className="w-100">
+          <h4 className="subtitles mt-2 text-center go-back-btn">
+            Torna indietro <ArrowReturnLeft />
+          </h4>
+        </Link>
         <div className="d-flex justify-content-center w-100">
           {searchParams.toString() !== "" && (
             <Button variant="outline-none" className="subtitles filter-btn" onClick={handleFilterReset}>
@@ -75,92 +81,94 @@ const ViewAllPage = () => {
           )}
           <FilterOffcanvas />
         </div>
-        <Table bordered hover striped className="mt-3">
-          <thead>
+        <div className="view-all-table-wrapper">
+          <Table bordered hover striped className="view-all-table mt-3 mx-auto">
+            <thead>
+              {isAnimalPage ? (
+                <tr>
+                  <th onClick={() => handleSorting("id")}>
+                    ID{" "}
+                    {animalSortByDirection === "asc" && animalSortBy === "id" ? (
+                      <SortNumericDown className="ms-2" />
+                    ) : animalSortByDirection === "desc" && animalSortBy === "id" ? (
+                      <SortNumericDownAlt className="ms-2" />
+                    ) : null}
+                  </th>
+                  <th onClick={() => handleSorting("name")}>
+                    Nome
+                    {animalSortByDirection === "asc" && animalSortBy === "name" ? (
+                      <SortAlphaDown className="ms-2" />
+                    ) : animalSortByDirection === "desc" && animalSortBy === "name" ? (
+                      <SortAlphaDownAlt className="ms-2" />
+                    ) : null}
+                  </th>
+                  <th>Età</th>
+                  <th>Sesso</th>
+                  <th>Specie</th>
+                  <th>Razza</th>
+                  <th>Status</th>
+                  <th>Città</th>
+                  <th>Provincia</th>
+                  <th>Regione</th>
+                  <th>Data di Ingresso</th>
+                  <th>Data del Rilascio</th>
+                  <th>Data del Decesso</th>
+                  <th>Cause del Decesso</th>
+                </tr>
+              ) : (
+                <tr>
+                  <th>ID</th>
+                  <th>Status</th>
+                  <th>Nome Utente</th>
+                  <th>Cognome Utente</th>
+                  <th>Animale</th>
+                  <th>Data della Richiesta</th>
+                  <th>Data di Inizio</th>
+                  <th>Data di Fine</th>
+                </tr>
+              )}
+            </thead>
             {isAnimalPage ? (
-              <tr>
-                <th style={{ width: "5%" }} onClick={() => handleSorting("id")}>
-                  ID{" "}
-                  {animalSortByDirection === "asc" && animalSortBy === "id" ? (
-                    <SortNumericDown className="ms-2" />
-                  ) : animalSortByDirection === "desc" && animalSortBy === "id" ? (
-                    <SortNumericDownAlt className="ms-2" />
-                  ) : null}
-                </th>
-                <th style={{ width: "8%" }} onClick={() => handleSorting("name")}>
-                  Nome
-                  {animalSortByDirection === "asc" && animalSortBy === "name" ? (
-                    <SortAlphaDown className="ms-2" />
-                  ) : animalSortByDirection === "desc" && animalSortBy === "name" ? (
-                    <SortAlphaDownAlt className="ms-2" />
-                  ) : null}
-                </th>
-                <th style={{ width: "3%" }}>Età</th>
-                <th style={{ width: "5%" }}>Sesso</th>
-                <th style={{ width: "5%" }}>Specie</th>
-                <th style={{ width: "5%" }}>Razza</th>
-                <th style={{ width: "7%" }}>Status</th>
-                <th style={{ width: "6.5%" }}>Città</th>
-                <th style={{ width: "6.5%" }}>Provincia</th>
-                <th style={{ width: "6%" }}>Regione</th>
-                <th style={{ width: "7%" }}>Data di Ingresso</th>
-                <th style={{ width: "8%" }}>Data del Rilascio</th>
-                <th style={{ width: "8%" }}>Data del Decesso</th>
-                <th style={{ width: "20%" }}>Cause del Decesso</th>
-              </tr>
+              <tbody>
+                {animalList.map(animal => (
+                  <tr key={animal.id}>
+                    <td>{animal.id}</td>
+                    <td>{animal.name}</td>
+                    <td>{animal.age}</td>
+                    <td>{animal.gender}</td>
+                    <td>{animal.species}</td>
+                    <td>{animal.breed}</td>
+                    {/* <td>{animal.description}</td>
+                  <td>{animal.clinicalCondition}</td> */}
+                    <td>{animal.status}</td>
+                    <td>{animal.city}</td>
+                    <td>{animal.province}</td>
+                    <td>{animal.region}</td>
+                    <td>{animal.entryDate ? new Date(animal.entryDate).toLocaleDateString("it-IT") : "Non disponibile."}</td>
+                    <td>{animal.releaseDate ? new Date(animal.releaseDate).toLocaleDateString("it-IT") : "Non disponibile."}</td>
+                    <td>{animal.deathDate ? new Date(animal.deathDate).toLocaleDateString("it-IT") : "Non disponibile."}</td>
+                    <td>{animal.deathCause}</td>
+                  </tr>
+                ))}
+              </tbody>
             ) : (
-              <tr>
-                <th>ID</th>
-                <th>Status</th>
-                <th>Nome Utente</th>
-                <th>Cognome Utente</th>
-                <th>Animale</th>
-                <th>Data della Richiesta</th>
-                <th>Data di Inizio</th>
-                <th>Data di Fine</th>
-              </tr>
+              <tbody>
+                {adoptionList.map(adoption => (
+                  <tr key={adoption.id}>
+                    <td>{adoption.id}</td>
+                    <td>{adoption.status}</td>
+                    <td>{adoption.user?.userName}</td>
+                    <td>{adoption.user?.userSurname}</td>
+                    <td>{adoption.animal?.name}</td>
+                    <td>{adoption.requestDate ? new Date(adoption.requestDate).toLocaleDateString("it-IT") : "Non disponibile"}</td>
+                    <td>{adoption.startDate ? new Date(adoption.startDate).toLocaleDateString("it-IT") : "Non disponibile"}</td>
+                    <td>{adoption.endDate ? new Date(adoption.endDate).toLocaleDateString("it-IT") : "Non disponibile"}</td>
+                  </tr>
+                ))}
+              </tbody>
             )}
-          </thead>
-          {isAnimalPage ? (
-            <tbody>
-              {animalList.map(animal => (
-                <tr key={animal.id}>
-                  <td>{animal.id}</td>
-                  <td>{animal.name}</td>
-                  <td>{animal.age}</td>
-                  <td>{animal.gender}</td>
-                  <td>{animal.species}</td>
-                  <td>{animal.breed}</td>
-                  {/* <td>{animal.description}</td>
-                <td>{animal.clinicalCondition}</td> */}
-                  <td>{animal.status}</td>
-                  <td>{animal.city}</td>
-                  <td>{animal.province}</td>
-                  <td>{animal.region}</td>
-                  <td>{animal.entryDate ? new Date(animal.entryDate).toLocaleDateString("it-IT") : "Non disponibile."}</td>
-                  <td>{animal.releaseDate ? new Date(animal.releaseDate).toLocaleDateString("it-IT") : "Non disponibile."}</td>
-                  <td>{animal.deathDate ? new Date(animal.deathDate).toLocaleDateString("it-IT") : "Non disponibile."}</td>
-                  <td>{animal.deathCause}</td>
-                </tr>
-              ))}
-            </tbody>
-          ) : (
-            <tbody>
-              {adoptionList.map(adoption => (
-                <tr key={adoption.id}>
-                  <td>{adoption.id}</td>
-                  <td>{adoption.status}</td>
-                  <td>{adoption.user?.userName}</td>
-                  <td>{adoption.user?.userSurname}</td>
-                  <td>{adoption.animal?.name}</td>
-                  <td>{adoption.requestDate ? new Date(adoption.requestDate).toLocaleDateString("it-IT") : "Non disponibile"}</td>
-                  <td>{adoption.startDate ? new Date(adoption.startDate).toLocaleDateString("it-IT") : "Non disponibile"}</td>
-                  <td>{adoption.endDate ? new Date(adoption.endDate).toLocaleDateString("it-IT") : "Non disponibile"}</td>
-                </tr>
-              ))}
-            </tbody>
-          )}
-        </Table>
+          </Table>
+        </div>
         {isAnimalPage ? (
           animalStatus === "pending" ? (
             <Button variant="outline-none" className="mt-4 load-more-btn mx-auto" disabled>
