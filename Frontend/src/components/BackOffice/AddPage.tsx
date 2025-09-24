cbd83b707690d83d119228d4c0f464e9e74fcfca
Dan Russo <rdaniele669@gmail.com>
2025-09-24 17:00:12 +0200

    adds go back btn to add and update back office page

--- Frontend/src/components/BackOffice/AddPage.tsx
+++ Frontend/src/components/BackOffice/AddPage.tsx
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
 import AnimalForm from "./AnimalForm";
 import ArticleForm from "./ArticleForm";
+import { Link } from "react-router-dom";
+import { ArrowReturnLeft } from "react-bootstrap-icons";
 
 const AddPage = () => {
   const isArticlePage = location.pathname.includes("/aggiungi/articoli");
@@ -10,6 +12,11 @@ const AddPage = () => {
       <Container id="back-office-add-section" className="navbar-height d-flex flex-column justify-content-start align-items-start information mb-4">
         <h2 className="titles mx-auto mb-2 mt-4">{isArticlePage ? "Articoli" : "Animali"}</h2>
         <h4 className="subtitles mx-auto">{isArticlePage ? "Modifica un articolo" : "Modifica un animale"}</h4>
+        <Link to={"/back-office"} className="w-100">
+          <h4 className="subtitles mt-2 text-center go-back-btn">
+            Torna indietro <ArrowReturnLeft />
+          </h4>
+        </Link>
         {isArticlePage ? <ArticleForm /> : <AnimalForm />}
       </Container>
     </>
