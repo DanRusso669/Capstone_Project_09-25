ae677b2f710aaef15fad0c78d29b7ae8f870f353
Dan Russo <rdaniele669@gmail.com>
2025-10-15 11:09:58 +0200

    adds monthly donation plan cards

--- Frontend/src/components/Donation/MonthlyDonationPage.tsx
+++ Frontend/src/components/Donation/MonthlyDonationPage.tsx
@@ -1,11 +1,31 @@
-import { Container } from "react-bootstrap";
-import RegisterForm from "../LoginAndRegister/RegisterForm";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./donation.css";
 
 const MonthlyDonationPage = () => {
+  const plans = [
+    {
+      name: "Sostenitore Base",
+      price: "10 €",
+      priceId: "price_1SIPu5E3YwyyMp3lRJLZWHzD",
+      feature: "Con questo piano riuscirai a salvare una vita all'anno.",
+    },
+    {
+      name: "Sostenitore Extra",
+      price: "20 €",
+      priceId: "price_1SIPuWE3YwyyMp3lyibKnlki",
+      feature: "Con questo piano riuscirai a salvare due vite all'anno.",
+    },
+    {
+      name: "Sostenitore Ultra",
+      price: "30 €",
+      priceId: "price_1SIPusE3YwyyMp3l7IM5YQ9D",
+      feature: "Con questo piano riuscirai a salvare tre vite all'anno.",
+    },
+  ];
+
   return (
     <>
-      <Container className="navbar-height d-flex flex-column justify-content-center align-items-center information mb-4">
+      <Container id="monthly-donation-section" className="navbar-height d-flex flex-column justify-content-center align-items-center information">
         <h1 className="titles mx-auto mb-2 mt-4">Sostieni il Rifugio Mamo</h1>
         <div className="monthly-donation-incipit text-center mx-2 mx-md-auto mb-4">
           <p>
@@ -21,10 +41,23 @@ const MonthlyDonationPage = () => {
         </div>
         <h2 className="subtitles mx-auto mb-2">Unisciti ad altri sostenitori</h2>
         <p className="mb-3">
-          Compila il seguente form per essere inserito tra i nostri sostenitori e ricordati che{" "}
-          <span className="fw-bold">è obbligatorio essere registrati</span> prima di poter continuare con la donazione mensile.
+          Scegli uno dei 3 possibili piani qui sotto e ricorda che qualsiasi tipo di donazione, anche la più piccola,{" "}
+          <span className="fw-bold">può fare la differenza</span>.
         </p>
-        <RegisterForm />
+        <Row className="mb-4 gap-3 justify-content-center">
+          {plans.map(plan => (
+            <Card key={plan.name} as={Col} xs={12} lg={4} className="text-center rounded-4 plan-card">
+              <Card.Body>
+                <Card.Title className="subtitles fs-3">{plan.name}</Card.Title>
+                <Card.Text className="fw-bold fst-italic fs-3">{plan.price}</Card.Text>
+                <Card.Text>{plan.feature}</Card.Text>
+                <Button className="monthly-payment-btn mt-3 mb-2" variant="outline-none">
+                  Sostieni
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
       </Container>
     </>
   );
